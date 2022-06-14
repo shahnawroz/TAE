9367879cf75c73e76c13ae8a9720929750d276ac
shahnawroz <sh.nawrose@gmail.com>
2022-06-14 15:03:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+const user = ["nawrose", "ratul", "saminvai"];
+
+app.get("/user/:name", (req, res) => {
+  
+  user.forEach((item, index) => {
+    if (req.params.name === item) {
+      res.json(item);
+    }
+    if (index + 1 === user.length) {
+      res.json("user not found");
+    }
+  });
+});
+
+
+app.listen(3000, () => console.log("Port is running at 3000"));
