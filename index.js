cd2b42f781cbc16270c664e6777b210483f30bd6
shahnawroz <sh.nawrose@gmail.com>
2022-06-23 00:25:23 +0600

    Port

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import bodyParser from "body-parser";
 import easyinvoice from "easyinvoice";
 import fs from "fs";
 
-
+const port= process.env.PORT || 3000;
 const app = express();
 
 
 app.use(bodyParser.json());
 const user = [ ];
-app.get ("/", (req,res)=>res.json("hello world"))
+
 
 app.post("/get-invoice", (req, res) => {
   //Import the library into your project
@@ -122,4 +122,4 @@ easyinvoice.createInvoice(data, async function (result) {
 
 
 
-app.listen(process.env.PORT || 3000, () => console.log("Port is running at 3000"));
+app.listen(port, () => console.log("Port is running at 3000"));
